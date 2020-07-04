ee780355170168a8ea7504f385b3e78a7ae71271
CodeRevenge <mas.arley2009@hotmail.com>
2020-07-04 11:56:38 -0500

    using localStorage for persistance

--- udemy_speed.js
+++ udemy_speed.js
@@ -1,12 +1,17 @@
 const SPEEDS_LIST_SELECTOR = "menu--playback-rate-menu--11hOW";
 const PARENT_VIDEO_SELECTOR =
   "div[data-purpose=curriculum-item-viewer-content]";
-const SPAN_CLASS_LIST = "playback-rate--playback-rate--1XOKO";
+const SPAN_CLASS_LIST = ["playback-rate--playback-rate--1XOKO"];
 const LI_CLASS_LIST = ["menu--menu--2Pw42", "menu--item--2IgLt"];
+const VIDEO_SPEED = "videoSpeed";
+
+let videoSpeed = parseFloat(localStorage.getItem(VIDEO_SPEED));
 
 const sleep = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
+
+const saveSpeed = (speed) => localStorage.setItem(VIDEO_SPEED, speed);
 
 const updateSpeed = (event, speed) => {
   video = document.getElementsByTagName("video")[0];
@@ -14,7 +19,7 @@ const updateSpeed = (event, speed) => {
   video.onplay = (e) => {
     e.target.playbackRate = speed;
   };
-  document.getElementById("playback-rate-menu").textContent = "" + speed;
+  document.getElementById("playback-rate-menu").textContent = speed;
   document.getElementsByClassName("active")[0].classList.remove("active");
   actual = event.target;
   if (actual.tagName === "SPAN") {
@@ -23,16 +28,18 @@ const updateSpeed = (event, speed) => {
     li = actual.parentNode;
   }
   li.classList.add("active");
+  videoSpeed = speed;
+  saveSpeed(speed);
 };
 
-const fillNewSpeeds = (list) => {
+const fillNewSpeeds = (list, video) => {
   let newSpeeds = [];
   for (i = 0.5; i <= 3.5; i += 0.25) {
     newSpeeds.push(i);
   }
   list.textContent = "";
   list.style.minWidth = "110px";
-  let actualSpeed = document.getElementsByTagName("video")[0].playbackRate;
+  let actualSpeed = video.playbackRate;
   newSpeeds.forEach((newSpeed) => {
     let li = document.createElement("LI");
     let a = document.createElement("A");
@@ -48,7 +55,7 @@ const fillNewSpeeds = (list) => {
     a.addEventListener("click", (e) => updateSpeed(e, newSpeed));
     a.addEventListener("onClick", (e) => updateSpeed(e, newSpeed));
     a.addEventListener("onKeyDown", (e) => updateSpeed(e, newSpeed));
-    span.classList.add(SPAN_CLASS_LIST);
+    span.classList.add(...SPAN_CLASS_LIST);
     span.appendChild(document.createTextNode(newSpeed));
     a.appendChild(span);
     li.appendChild(a);
@@ -56,7 +63,7 @@ const fillNewSpeeds = (list) => {
   });
 };
 
-const videoChanged = (e) => {
+const videoChanged = (e, speed) => {
   let childList = e.filter(
     (i) =>
       i.type === "childList" &&
@@ -64,12 +71,12 @@ const videoChanged = (e) => {
       i.addedNodes[0].tagName === "VIDEO"
   );
   if (childList.length > 0) {
-    udemySpeeds();
+    udemySpeeds(speed);
   }
 };
 
-const udemySpeeds = async () => {
-  console.log("Udemy Speed Working...");
+const udemySpeeds = async (speed = 1) => {
+  console.log(`Udemy Speed Working... Speed set to ${speed}`);
 
   let list;
   do {
@@ -77,16 +84,25 @@ const udemySpeeds = async () => {
     list = document.getElementsByTagName("ul")[1];
   } while (list != undefined && !list.classList.contains(SPEEDS_LIST_SELECTOR));
   let div = document.querySelector(PARENT_VIDEO_SELECTOR);
-  let observer = new MutationObserver(videoChanged);
+  const video = document.querySelector("video");
+  video.playbackRate = speed;
+  video.onplay = (e) => {
+    e.target.playbackRate = speed;
+  };
+  document.getElementById("playback-rate-menu").textContent = speed;
+  videoSpeed = speed;
+  let observer = new MutationObserver((e) => videoChanged(e, videoSpeed));
   observer.observe(div, {
     childList: true,
     attributes: true,
     characterData: true,
     subtree: true,
   });
-  if (list.getElementsByTagName("li").length <= 7) fillNewSpeeds(list);
-
-  // div.addEventListener("DOMNodeInserted", videoChanged);
+  if (list.getElementsByTagName("li").length <= 7) fillNewSpeeds(list, video);
 };
 
-udemySpeeds();
+if (videoSpeed) {
+  udemySpeeds(videoSpeed);
+} else {
+  udemySpeeds();
+}
